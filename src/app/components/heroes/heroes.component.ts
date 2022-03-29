2c12a8f47c0a52efca25d26096b26a761f7c94e6
koz-y <77233172+koz-y@users.noreply.github.com>
2022-03-29 17:26:24 +0900

    Get data from a server

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -12,7 +12,7 @@ import { Hero } from '../../hero';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
+  // selectedHero?: Hero;
 
   constructor(private heroService: HeroService) {}
 
@@ -24,5 +24,20 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe((data) => {
       this.heroes = data;
     });
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter((h) => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
   }
 }
